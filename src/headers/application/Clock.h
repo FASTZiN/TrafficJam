8ffec5933c60b25bbf70cfe8da02a258fe751912
FASTZiN <caiogiesteria@hotmail.com>
2017-09-22 17:17:00 -0300

    XXXXXXXXXXXX

--- src/headers/application/Clock.h
+++ src/headers/application/Clock.h
@@ -1,8 +1,21 @@
 #ifndef HEADERS_CLOCK_H_
 #define HEADERS_CLOCK_H_
 
+#include "./Clock.h"
+#include <cstdint>
+#include <stdexcept>
+#include <chrono>
 
+class Clock{
+ public:
+	Clock();
+	~Clock();
+	void carArrivel();
+	void semaphoreChange();
+	void changeTrack();
+	void semaphoreArrivel();
+ private:
+	auto time;
 
-
-
+};
 #endif
---
+++ src/managers/Clock.cpp
@@ -0,0 +1,26 @@
+#include "./Clock.h"
+
+ Clock::Clock(){
+
+}
+
+ Clock::~Clock(){
+
+ }
+
+ void Clock::carArrivel(){
+
+ }
+
+ void Clock::semaphoreChange(){
+
+  }
+
+ void Clock::changeTrack(){
+
+  }
+
+ void Clock::semaphoreArrivel(){
+
+  }
+
