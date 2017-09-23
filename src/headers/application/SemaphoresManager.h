268745fde16193899402873a0c87f2b5df835e64
DavidGrunheidt <davidordine@hotmail.com>
2017-09-23 14:49:26 -0300

    Semaphores manager done

--- src/headers/application/SemaphoresManager.h
+++ src/headers/application/SemaphoresManager.h
@@ -6,18 +6,18 @@
 class SemaphoresManager {
 public:
 
-	SemaphoresManager();
-	void createSemaphore(std::string name, int green_time, int red_time, Track track_in, Track left,Track straight , Track right, int left, int straight, int right);
+	SemaphoresManager();  //  Construtor do Gerenciador de Semaforos
+	void createSemaphore(std::string name, int green_time, int red_time, int pointing, Track track_in, Track left,Track straight , Track right, int left, int straight, int right);
 	void changeState();
 	Semaphore getSpecificSemaphore(std::string name);
 	void nextStage();
-	int getActualState();
+	int getActualStage();
 
 private:
 
 	structures::LinkedList<Track> all_semaphores;
-	int actual_state;  //  All { 0 = North Open, 1 = East Open, 2 == South Open, 3 == Least Open } Rest = Close, Others ==  All Close;
-	void openPointerSem(int pointing);
+	int actual_stage;  //  All { 0 = North Open, 1 = East Open, 2 == South Open, 3 == Least Open } Rest = Close, Others ==  All Close;
+	void openPointersSem(int pointing);
 };
 
 #endif
--- src/managers/SemaphoresManager.cpp
+++ src/managers/SemaphoresManager.cpp
@@ -2,51 +2,58 @@
 
 SemaphoresManager::SemaphoresManager() {
 	all_semaphores = new structures::LinkedList<Track>();
+	actual_stage = 4;
 }
 
-void SemaphoresManager::createSemaphore(std::string name, int green_time,int red_time, Track track_in, Track left_track, Track straight_track,Track right_track, int left, int straight, int right) {
-	Semaphore semaphore = new Semaphore(name, green_time, red_time);
+void SemaphoresManager::createSemaphore(std::string name, int green_time,int red_time, int pointing, Track track_in, Track left_track, Track straight_track,Track right_track, int left, int straight, int right) {
+	Semaphore semaphore = new Semaphore(name, green_time, red_time, pointing);
 	semaphore.setTrackIn(track_in);
 	semaphore.setTracksOut(left_track, straight_track, right_track);
 	semaphore.setPossibilities(left, straight, right);
 }
 
 void SemaphoresManager::changeState() {
-	switch (actual_state) {
+	switch (actual_stage) {
 		default:
-			this->openPointerSem(4);
+			this->openPointersSem(4);
 			break;
 		case 0:
-			this->openPointerSem(0);
+			this->openPointersSem(0);
 			break;
 		case 1:
-			this->openPointerSem(1);
+			this->openPointersSem(1);
 			break;
 		case 2:
-			this->openPointerSem(2);
+			this->openPointersSem(2);
 			break;
 		case 3:
-			this->openPointerSem(3);
+			this->openPointersSem(3);
 			break;
 	}
 }
 
 Semaphore SemaphoresManager::getSpecificSemaphore(std::string name) {
-
+	for (int i = 0; i < all_semaphores.size(); i++) {
+		Semaphore semaphore = all_semaphores.at(i);
+		if (semaphore.getName() == name) {
+			return semaphore;
+			break;
+		}
+	}
+	throw std::out_of_range ("There's no semaphore with this name");  // Caso percorra todo o for significa que não achou nenhum com o nome
 }
 
-void SemaphoresManager::nextState() {
-	if(actual_state+1 == 5)
-		actual_state = 0;
-	else
-		actual_state++;
+void SemaphoresManager::nextStage() {
+	actual_stage++;
+	if (actual_stage == 5)
+		actual_stage = 0;
 }
 
-int SemaphoresManager::getActualState() {
-
+int SemaphoresManager::getActualStage() {
+	return actual_stage;
 }
 
-void SemaphoresManager::openPointerSem(int pointing)  {
+void SemaphoresManager::openPointersSem(int pointing)  {
 	for (int i = 0; i < all_semaphores.size(); i++) {
 		Semaphore semaphore = all_semaphores.at(i);
 		if (semaphore.getPointing() == pointing)
