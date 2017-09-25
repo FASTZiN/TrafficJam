0f585b932f1c6b0e62f6326d2a6b6fcc0f0fd8e5
FASTZiN <caiogiesteria@hotmail.com>
2017-09-25 11:46:05 -0300

    FODASE

--- src/sources/Event.cpp
+++ src/sources/Event.cpp
@@ -7,19 +7,19 @@
 	}
 
 	void Event::vehicleBorns(Track &track, Vehicle &vehicle) {
-		std::string vehicle_borns_event= ("Vehicle %d was born in track in time");
+		std::string vehicle_borns_event= ("Vehicle %p was born in track %s in time %d", vehicle, track.getName(), time_of_event);
 	}
 
 	void Event::semaphoreStateChange(Semaphore &semaphore) {
-		std::string semaphore_state_event= "";
+		std::string semaphore_state_event= ("Semaphore %p has change state to %d in time %d", semaphore, semaphore.getState(), time_of_event);
 	}
 
 	void Event::vehicleIsInSemaphore(Track &track, Vehicle &vehicle) {
-		std::string vehicle_on_semaphore_event= "";
+		std::string vehicle_on_semaphore_event= ("Vehicle %p was in semaphore on track %s in time %d", vehicle, track.getName(), time_of_event);
 	}
 
 	void Event::vehicleChangedTrack(Track &track, Vehicle &vehicle) {
-		std::string vehicle_change_track= "";
+		std::string vehicle_change_track= ("Vehicle %p change track to &s in time %d", vehicle, track.getName(), time_of_event);
 	}
 
 	structures::LinkedList<std::string> Event::getEventList() {
